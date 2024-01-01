2d9f877c2f96187dbb80dda8b55194d9e94a6e00
seo688 <seo688@gmail.com>
2024-01-01 23:49:07 +0900

    작업내용 수정2

--- 작업내용_01.sql
+++ 작업내용_01.sql
@@ -150,3 +150,92 @@ public class JpaConfig {
 }
 
 
+@Data
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AccountMeResponse {
+
+    private String email;
+
+    private String name;
+
+    private LocalDateTime registerdAt;
+}
+
+
+@GetMapping("/me")
+public AccountMeResponse save(){
+
+    var accRes = AccountMeResponse.builder()
+            .name("홍길동")
+            .email("hong@gmail.com")
+            .registerdAt(LocalDateTime.now())
+            .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    Map result = objectMapper.convertValue(accRes, Map.class);
+
+    log.info("dto to map: {}", result);
+
+    return accRes;
+
+}
+
+
+@Data
+@Builder
+//@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AccountMeResponse {
+
+    private String email;
+
+    private String name;
+
+//    @JsonSerialize(using = LocalDateTimeSerializer.class)
+//    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+//    private LocalDateTime registerdAt;
+
+    private LocalDateTime registerdAt;
+}
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper);
+    }
+}
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new Jdk8Module());  // jdk 8 버전 이후 클래스
+
+        objectMapper.registerModule(new JavaTimeModule());  // << local date
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        //snake case
+        objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+
+        return objectMapper;
+    }
+}
+
+@Configuration
+@EntityScan(basePackages = "org.delivery.db")
+@EnableJpaRepositories(basePackages = "org.delivery.db")
+public class JpaConfig {
+}
+
